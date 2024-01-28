6210d9f606f1be44dfd999c769baba3cc82af849
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2024-01-28 15:15:51 +0800

    feat: add controller for create blog

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/veoscript/go-gin-crud/controllers"
 	"github.com/veoscript/go-gin-crud/initializers"
 )
 
@@ -12,10 +13,6 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/blogs", controllers.BlogsCreate)
 	r.Run()
 }
