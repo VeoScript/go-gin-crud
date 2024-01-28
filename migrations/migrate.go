1064b1d3ef67c89fd676e65c2cbbac727ec65ebf
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2024-01-28 14:40:24 +0800

    feat: add mode and migration for blog

---
+++ migrations/migrate.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/veoscript/go-gin-crud/initializers"
+	"github.com/veoscript/go-gin-crud/models"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	var DB = initializers.DB
+	DB.AutoMigrate(&models.Blog{})
+}
