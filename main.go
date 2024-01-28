b8eabcdcd3b83c5d195441ea175511c3b8afcc32
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2024-01-28 14:24:50 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/veoscript/go-gin-crud/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
